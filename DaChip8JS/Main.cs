2ac15706d1b33e723c11516834f981092ff8922d
Danny Tuppeny <danny@tuppeny.com>
2016-06-11 12:33:31 +0100

    Add rendering code.
    Oops, missed from previous commit!

--- DaChip8JS/Main.cs
+++ DaChip8JS/Main.cs
@@ -1,5 +1,5 @@
-﻿using System.Collections.Generic;
-using System.Diagnostics;
+﻿using System;
+using System.Collections.Generic;
 using Bridge.Html5;
 
 namespace DanTup.DaChip8JS
@@ -12,15 +12,30 @@ namespace DanTup.DaChip8JS
 		static readonly int targetElapsedTime60Hz = (int)(1000f / 60); // 60 tickets per second
 		static readonly int targetElapsedTime = (int)(1000f / 500); // 500 ticks per second
 
+		static HTMLCanvasElement screen;
+		static CanvasRenderingContext2D screenContext;
+		static ImageData darkPixel, lightPixel;
+
 		[Ready]
 		public static void OnReady()
 		{
+			// Set up canvas rendering.
+			screen = Document.GetElementById<HTMLCanvasElement>("screen");
+			screenContext = screen.GetContext(CanvasTypes.CanvasContext2DType.CanvasRenderingContext2D);
+			darkPixel = screenContext.CreateImageData(1, 1);
+			lightPixel = screenContext.CreateImageData(1, 1);
+			lightPixel.Data[1] = 0x64; // Set green part (#006400)
+			darkPixel.Data[3] = lightPixel.Data[3] = 255; // Alpha
+
+			// Create the interpreter.
 			chip8 = new Chip8(Draw, Beep);
 
-			BeginLoadRom(ROM);
-
+			// Pass keypresses over to the interpreter.
 			Document.OnKeyUp += SetKeyDown;
 			Document.OnKeyDown += SetKeyUp;
+
+			// Kick off async loading of ROM.
+			BeginLoadRom(ROM);
 		}
 
 		static void BeginLoadRom(string rom)
@@ -46,6 +61,9 @@ namespace DanTup.DaChip8JS
 
 		static void Draw(bool[,] buffer)
 		{
+			for (var x = 0; x < buffer.GetLength(0); x++)
+				for (var y = 0; y < buffer.GetLength(1); y++)
+					screenContext.PutImageData(buffer[x, y] ? lightPixel : darkPixel, x, y);
 		}
 
 		static void Beep(int milliseconds)
